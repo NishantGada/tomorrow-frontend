8583baa08e07410d1a20605e1bf80598fabcf8e7
NishantGada <nishant.gada160@gmail.com>
2025-09-19 01:35:19 -0400

    refactor: integrates fetch tasks api + testing some new code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,114 +1,82 @@
 import { View, Text, StyleSheet, ScrollView, SafeAreaView } from 'react-native'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Date from './Date'
 import TaskCard from './TaskCard'
 import { StyleConstants } from '../../utils/StyleConstants';
+import SendRequest from '../../utils/SendRequest';
+// import { GlobalConstants } from '../../utils/GlobalConstants';
+// import { formatDateSimple } from '../../utils/GetDate';
 
 export default function Home({ navigation }) {
-	const [tasks, setTasks] = useState([
-		{
-			"id": 1,
-			"title": "task 1",
-			"description": "task 1 description",
-			"priority": "high"
-		},
-		{
-			"id": 2,
-			"title": "task 2",
-			"description": "task 2 description",
-			"priority": "high"
-		},
-		{
-			"id": 3,
-			"title": "task 3",
-			"description": "task 3 description",
-			"priority": "high"
-		},
-		{
-			"id": 4,
-			"title": "task 4",
-			"description": "task 4 description",
-			"priority": "medium"
-		},
-		{
-			"id": 5,
-			"title": "task 5",
-			"description": "task 5 description",
-			"priority": "medium"
-		},
-		{
-			"id": 6,
-			"title": "task 6",
-			"description": "task 6 description",
-			"priority": "medium"
-		},
-		{
-			"id": 7,
-			"title": "task 7",
-			"description": "task 7 description",
-			"priority": "low"
-		},
-		{
-			"id": 8,
-			"title": "task 8",
-			"description": "task 8 description",
-			"priority": "low"
-		},
-		{
-			"id": 9,
-			"title": "task 9",
-			"description": "task 9 description",
-			"priority": "low"
-		},
-	])
+  const [tasks, setTasks] = useState([])
 
-	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor: "white", }}>
-			<ScrollView style={styles.container} contentContainerStyle={styles.scrollViewStyle} showsVerticalScrollIndicator={false}>
-				<View style={styles.header}>
-					<View>
-						<Text style={styles.heading}>What's</Text>
-						<Text style={styles.heading2}>Tomorrow?</Text>
-					</View>
-					<Date />
-				</View>
+  const getTasksAPI = async () => {
+    try {
+      const response = await SendRequest("/tasks", {}, "GET", {});
+      console.log("response: ", response.data.data.tasks);
+      setTasks(...tasks, response.data.data.tasks)
 
-				<TaskCard category={StyleConstants["high"]} tasks={tasks.filter(task => task.priority === "high")} setTasks={setTasks} />
-				<TaskCard category={StyleConstants["medium"]} tasks={tasks.filter(task => task.priority === "medium")} setTasks={setTasks} />
-				<TaskCard category={StyleConstants["low"]} tasks={tasks.filter(task => task.priority === "low")} setTasks={setTasks} />
-			</ScrollView>
-		</SafeAreaView>
-	)
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  // const today = new Date();
+  // const todayFormatted = formatDateSimple(today);
+  // const todaysTasks = tasks.filter(task => task.date_added === todayFormatted);
+  // console.log({ todaysTasks });
+
+  useEffect(() => {
+    getTasksAPI();
+  }, [])
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white", }}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.scrollViewStyle} showsVerticalScrollIndicator={false}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.heading}>What's</Text>
+            <Text style={styles.heading2}>Tomorrow?</Text>
+          </View>
+          <Date />
+        </View>
+
+        <TaskCard category={StyleConstants["high"]} tasks={tasks.filter(task => task.priority === "high")} setTasks={setTasks} />
+        <TaskCard category={StyleConstants["medium"]} tasks={tasks.filter(task => task.priority === "medium")} setTasks={setTasks} />
+        <TaskCard category={StyleConstants["low"]} tasks={tasks.filter(task => task.priority === "low")} setTasks={setTasks} />
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		paddingTop: 20,
-		paddingHorizontal: 20,
-	},
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
 
-	scrollViewStyle: {
-		paddingBottom: 100
-	},
+  scrollViewStyle: {
+    paddingBottom: 100
+  },
 
-	header: {
-		display: "flex",
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "space-between",
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
 
-		marginBottom: 20,
-	},
+    marginBottom: 20,
+  },
 
-	heading: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 
-	heading2: {
-		fontSize: 36,
-		fontWeight: "bold",
-		marginBottom: 10
-	},
+  heading2: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
 })
