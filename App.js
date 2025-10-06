932d6fcea560e98677269e07131569011d2cfc60
NishantGada <nishant.gada160@gmail.com>
2025-10-05 21:15:20 -0400

    refactor: adds modal to app.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -9,23 +9,33 @@ import Profile from './src/pages/Profile/Profile';
 import Appbar from './src/components/Appbar/Appbar';
 import Search from './src/pages/Search/Search';
 import Previous from './src/pages/Previous/Previous';
+import AddNewModal from './src/components/Modal/AddNewModal';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <SafeAreaProvider style={styles.safeArea}>
       <NavigationContainer>
         <Tab.Navigator
-          tabBar={props => <Appbar {...props} />}
+          tabBar={props => (
+            <Appbar
+              {...props}
+              onAddPress={() => setVisible(true)}
+            />
+          )}
           screenOptions={{ headerShown: false }}
         >
           <Tab.Screen name="Home" component={Home} />
-          {/* <Tab.Screen name="Search" component={Search} /> */}
-          <Tab.Screen name="Previous" component={Previous} />
+          {/* <Tab.Screen name="Search" component={Search} />
+          <Tab.Screen name="Previous" component={Previous} /> */}
           <Tab.Screen name="Profile" component={Profile} />
         </Tab.Navigator>
       </NavigationContainer>
+
+      <AddNewModal visible={visible} setVisible={setVisible} />
     </SafeAreaProvider>
   );
 }
