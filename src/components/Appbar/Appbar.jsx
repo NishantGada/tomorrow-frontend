c2577072356b432fdc9a712952a916125e81ea2c
NishantGada <nishant.gada160@gmail.com>
2025-10-05 21:13:39 -0400

    refactor: updates app bar

--- src/components/Appbar/Appbar.jsx
+++ src/components/Appbar/Appbar.jsx
@@ -1,67 +1,99 @@
 // src/components/CustomAppBar.js
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import AddNewModal from '../Modal/AddNewModal';
 
-export default function Appbar({ state, descriptors, navigation }) {
-	return (
-		<View style={styles.container}>
-			{state.routes.map((route, index) => {
-				const focused = state.index === index;
-				const { options } = descriptors[route.key];
-				const iconName = {
-					Home: focused ? 'home' : 'home-outline',
-					Profile: focused ? 'person' : 'person-outline',
-					Search: focused ? 'search' : 'search-outline',
-					History: focused ? 'time' : 'time-outline',
-				}[route.name] || 'ellipse';
+export default function Appbar({ state, descriptors, navigation, onAddPress }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.appBar}>
+        {state.routes.map((route, index) => {
+          const focused = state.index === index;
+          const { options } = descriptors[route.key];
+          const iconName = {
+            Home: focused ? 'home' : 'home-outline',
+            Profile: focused ? 'person' : 'person-outline',
+            Search: focused ? 'trending-up-sharp' : 'trending-up-sharp',
+            History: focused ? 'play-back' : 'play-back-outline',
+          }[route.name] || 'ellipse';
 
-				const onPress = () => {
-					if (!focused) navigation.navigate(route.name);
-				};
+          const onPress = () => {
+            if (!focused) navigation.navigate(route.name);
+          };
 
-				return (
-					<TouchableOpacity
-						key={route.key}
-						onPress={onPress}
-						style={styles.iconContainer}
-					>
-						<Ionicons
-							name={iconName}
-							size={28}
-							color={focused ? "black" : '#8e8e93'}
-						/>
-					</TouchableOpacity>
-				);
-			})}
-		</View>
-	);
+          return (
+            <TouchableOpacity
+              key={route.key}
+              onPress={onPress}
+              style={styles.iconContainer}
+            >
+              <Ionicons
+                name={iconName}
+                size={28}
+                color={focused ? "black" : '#8e8e93'}
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <TouchableOpacity onPress={onAddPress} style={styles.addButtonContainer}>
+        <Text style={styles.addButton}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		position: 'absolute',
-		bottom: 20,
-		
-		flexDirection: 'row',
-		alignSelf: 'center',
-		alignItems: 'center',
-		justifyContent: 'space-around',
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 20,
+    justifyContent: "center",
+    columnGap: 10
+  },
 
-		backgroundColor: "white",
-		borderRadius: 10,
-		width: "80%",
-		height: 70,
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 8 },
-		shadowOpacity: 0.1,
-		shadowRadius: 10,
-		elevation: 5,
-		borderWidth: 1.5,
-		borderColor: "black"
-	},
-	iconContainer: {
-		flex: 1,
-		alignItems: 'center',
-	},
+  appBar: {
+    // position: 'absolute',
+    // bottom: 20,
+    // left: 10,
+
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "70%",
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+    borderWidth: 1.5,
+    borderColor: "black"
+  },
+
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  addButtonContainer: {
+    fontSize: "100",
+    borderWidth: 1.5,
+    borderColor: "black",
+    borderRadius: 10,
+    width: 60,
+    height: 60,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  addButton: {
+    fontSize: "30",
+  },
 });
