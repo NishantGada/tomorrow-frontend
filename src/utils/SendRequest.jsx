568ff2f36aee49b62cfcfa9d778b30674b9e80c3
NishantGada <nishant.gada160@gmail.com>
2025-10-19 20:47:34 -0400

    chore: updates SendRequest spacing

--- src/utils/SendRequest.jsx
+++ src/utils/SendRequest.jsx
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
 const SendRequest = (path, data, method = 'POST', headers = {}) => {
-	path = "http://10.0.0.97:8080" + path;
-	const username = 'ng@gmail.com';
-	const password = 'qweqwe';
-	const token = btoa(`${username}:${password}`);
+  path = "http://10.0.0.97:8080" + path;
+  const username = 'ng@gmail.com';
+  const password = 'qweqwe';
+  const token = btoa(`${username}:${password}`);
 
-	headers = {
-		...headers,
-		Authorization: `Basic ${token}`,
-		'Content-Type': 'application/json'
-	}
+  headers = {
+    ...headers,
+    Authorization: `Basic ${token}`,
+    'Content-Type': 'application/json'
+  }
 
-	switch (method) {
-		case 'POST':
-			return axios.post(path, data, { headers });
-		case 'GET':
-			return axios.get(path, { headers });
-		case 'PUT':
-			return axios.put(path, data, { headers });
-		case 'DELETE':
-			return axios.delete(path, { headers });
-		default:
-			throw new Error(`Unsupported method: ${method}`);
-	}
+  switch (method) {
+    case 'POST':
+      return axios.post(path, data, { headers });
+    case 'GET':
+      return axios.get(path, { headers });
+    case 'PUT':
+      return axios.put(path, data, { headers });
+    case 'DELETE':
+      return axios.delete(path, { headers });
+    default:
+      throw new Error(`Unsupported method: ${method}`);
+  }
 };
 
 export default SendRequest;
