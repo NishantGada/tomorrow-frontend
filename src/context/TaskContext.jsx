98819c25b63978192a2eb2a0c650182b44b4147b
NishantGada <nishant.gada160@gmail.com>
2025-10-27 20:26:28 -0400

    refactor: updates TaskContext to use loggedInUser

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,26 +1,32 @@
 import React, { createContext, useEffect, useState } from 'react';
 import SendRequest from '../utils/SendRequest';
+import { useAuth } from './AuthContext';
 
 export const TaskContext = createContext();
 
 export const TaskContextProvider = ({ children }) => {
-  const [contextTasks, setContextTasks] = useState({});
-  const [reload, setReload] = useState(false);
+  const [contextTasks, setContextTasks] = useState([]);
+  const [reloadTasks, setReloadTasks] = useState(false);
+
+  const { loggedInUser } = useAuth();
 
   const getTasksAPI = async () => {
+    console.log("inside getTasksAPI");
     try {
-      const response = await SendRequest("/tasks", {}, "GET", {});
+      const response = await SendRequest("/tasks", {}, loggedInUser, "GET", {});
       setContextTasks(response.data.data.tasks);
     } catch (error) {
       console.log("getTasksAPI error:", error);
     }
   };
 
-  const refreshTasks = () => setReload(prev => !prev);
+  const refreshTasks = () => setReloadTasks(prev => !prev);
 
   useEffect(() => {
-    getTasksAPI();
-  }, [reload]);
+    if (loggedInUser && Object.keys(loggedInUser).length > 0) {
+      getTasksAPI();
+    }
+  }, [reloadTasks, loggedInUser]);
 
   return (
     <TaskContext.Provider value={{ contextTasks, setContextTasks, refreshTasks }}>
